e2bae7346c6017c693438ed75ee31fda75ae3f18
mandynhaaa <amandatobler18@gmail.com>
2026-07-05 23:33:23 -0300

    feat: setup fastify server with initial application routes

---
+++ src/infra/http/server.ts
@@ -0,0 +1,104 @@
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import { env } from '../../env.js';
+import { createShortUrl } from '../../app/functions/create-short-url.js';
+import { z } from 'zod';
+import { getOriginalUrl } from '../../app/functions/get-original-url.js';
+import { listLinks } from '../../app/functions/list-links.js';
+import { deleteLink } from '../../app/functions/delete-link.js';
+import { exportLinksCsv } from '../../app/functions/export-links-csv.js';
+
+const app = fastify()
+
+await app.register(cors, {
+  origin: '*',
+})
+
+app.post('/create-link', async (request, reply) => {
+  const createLinkBodySchema = z.object({
+    originalUrl: z.string().url('Invalid URL format'),
+  })
+
+  const result = createLinkBodySchema.safeParse(request.body);
+  
+  if (!result.success) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      errors: result.error.format(),
+    })
+  }
+
+  const { originalUrl } = result.data;
+
+  try {
+    const link = await createShortUrl({ originalUrl });
+    return reply.status(201).send(link);
+  } catch (error: any) {
+    return reply.status(400).send({ message: error.message });
+  }
+});
+
+app.get('/:code', async (request, reply) => {
+  const getLinkParamsSchema = z.object({
+    code: z.string(),
+  })
+
+  const { code } = getLinkParamsSchema.parse(request.params);
+
+  try {
+    const { originalUrl } = await getOriginalUrl({ code });
+    
+    return reply.status(200).send({ originalUrl });
+  } catch (error: any) {
+    return reply.status(404).send({ message: error.message });
+  }
+});
+
+app.get('/list-links', async (request, reply) => {
+  const listLinksQuerySchema = z.object({
+    page: z.number().optional().default(1),
+    limit: z.number().optional().default(20),
+  });
+
+  const { page, limit } = listLinksQuerySchema.parse(request.query);
+
+  try {
+    const { links } = await listLinks({ page, limit })
+    return reply.status(200).send({ links });
+  } catch (error: any) {
+    return reply.status(400).send({ message: error.message });
+  }
+});
+
+app.delete('/:code', async (request, reply) => {
+  const deleteLinkParamsSchema = z.object({
+    code: z.string(),
+  });
+
+  const { code } = deleteLinkParamsSchema.parse(request.params)
+
+  try {
+    await deleteLink({ code })
+    return reply.status(204).send()
+  } catch (error: any) {
+    return reply.status(404).send({ message: error.message })
+  }
+});
+
+app.post('/export', async (request, reply) => {
+  try {
+    const { downloadUrl } = await exportLinksCsv()
+    return reply.status(200).send({ downloadUrl })
+  } catch (error: any) {
+    return reply.status(500).send({ message: error.message })
+  }
+});
+
+app
+  .listen({
+    port: env.PORT,
+    host: 'localhost',
+  })
+  .then(() => {
+    console.log(`🚀 HTTP Server running on http://localhost:${env.PORT}`)
+  })
